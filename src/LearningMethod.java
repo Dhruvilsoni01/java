cc4c02788ec37d0e9588a7bb6324a64a57df12df
Dhruvilsoni01 <qadhruvil@gmail.com>
2023-07-11 21:38:39 +0530

    Updated code in LearnMethod.java

--- src/LearningMethod.java
+++ src/LearningMethod.java
@@ -1,19 +1,27 @@
 
+
 public class LearningMethod {
 
 	int i;
 	int j;
 	
 	public void show() {
-		int a;
-		int b;
-		String c;
-		System.out.println("Show method called");
+		int a=1;
+		int b=2;
+		int sum = a +b;
+		String message = "Summation is " + sum; 
+		System.out.println(message);
+		this.display(a,message,'a',3.2f,true);
 	}
 	public int display(int k,String a,char c,float f,boolean b)//declare
 	{
 		int j=200;
-		System.out.println("Hello World");
+		System.out.println("Hello World " + j);
+		System.out.println("Receive interger value: "+ k);
+		System.out.println("String value received: "+ a);
+		System.out.println("Character received value:" + c);
+		System.out.println("Float value received: " + f);
+		System.out.println("Boolean value received:" + b);
 		return 10;
 		
 	}

26e8a43795e7ef190e5750263a297a3b2ebd93f2
Dhruvilsoni01 <qadhruvil@gmail.com>
2023-07-11 21:06:50 +0530

    My second commit by own with LearningMethod.java

---
+++ src/LearningMethod.java
@@ -0,0 +1,25 @@
+
+public class LearningMethod {
+
+	int i;
+	int j;
+	
+	public void show() {
+		int a;
+		int b;
+		String c;
+	}
+	public int display(int k,String a,char c,float f,boolean b)//declare
+	{
+		int j=200;
+		System.out.println("Hello World");
+		return 10;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LearningMethod learn=new LearningMethod(); // Create the object of class.
+		learn.show(); // Call created method to use object
+	}
+
+}

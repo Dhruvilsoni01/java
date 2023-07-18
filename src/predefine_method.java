e5c05da61d017b297e7723d71e337ea24eeb7f52
Dhruvilsoni01 <qadhruvil@gmail.com>
2023-07-18 07:16:06 +0530

    Predefine Method
    Example of Predefine methods in java

---
+++ src/predefine_method.java
@@ -0,0 +1,15 @@
+
+public class predefine_method {
+	
+	public static void main(String[] args) {
+		// Every time it will generate the random number
+		
+		double num= Math.random();
+		System.out.println("Random number is : " + num);
+
+		// Type casting
+		int num1=(int)(Math.random()*100);
+		System.out.println("After type casting value is :"+num1);
+	}
+
+}

5dceb83ebd47611d6322ca27b0031ff45047fb99
Dhruvilsoni01 <qadhruvil@gmail.com>
2023-07-22 14:00:44 +0530

    Create Dependency

---
+++ src/Switchcase.java
@@ -0,0 +1,46 @@
+
+public class Switchcase {
+
+	public static void main(String[] args) {
+		// Switch Statement
+		
+		int day =3;
+		
+		switch (day) {
+		case 1: {
+			
+			System.out.println("Monday");;
+		}
+
+case 2: {
+			
+			System.out.println("Tuesday");;
+		}
+case 3: {
+	
+	System.out.println("Wednesday");
+}
+case 4: {
+	
+	System.out.println("Thursday");
+}
+case 5: {
+	
+	System.out.println("Friday");
+}
+	case 6: {
+	
+	System.out.println("Saturday");;
+}
+	case 7: {
+	
+	System.out.println("Sunday");;
+}
+
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + day);
+		}
+
+	}
+
+}
